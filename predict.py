5dd27a9c167797ce200388ceed33eb72cef8c93c
潘纪情 <panjiqing@192.168.1.15>
2022-09-19 00:04:38 +0800

    modify predict obs

--- predict.py
+++ predict.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import json
 from predictobstacle import PredictObstacle
+from predictobstacle import SyncPredictObstacle
+import queue
 
 data_path = "/Users/panjiqing/Desktop/licode/data/predict_data/_viz_prediction_pred_obstacles.csv"
 
@@ -10,18 +12,41 @@ data_path = "/Users/panjiqing/Desktop/licode/data/predict_data/_viz_prediction_p
 
 with open(data_path, 'r',encoding='utf-8-sig') as f:
     # 循环每行
+    label_list = []
     for line in f:
-        # 将每行处理后为列表，再添加到data中
         if line != '_viz_prediction_pred_obstacles\n':
             # print(json.loads(line)["markers"])
             data = json.loads(line)["markers"]
             if len(data) > 0:
+                syncobs = SyncPredictObstacle(3)
                 for obs in data:
-                    print(PredictObstacle(obs).label)
+                    pred_obs = PredictObstacle(obs)
+                    pred_obs.get_id_time()
+                    if pred_obs.id_time == None :
+                        del pred_obs
+                        continue
+                    else:
+                        syncobs.add_predict_obs(pred_obs)
+                    print(pred_obs.id)
+                    print('time:' , pred_obs.id_time)
+                    # print('timestamp:' , pred_obs.timestamp)
+                    # if predobs.label not in label_list:
+                    # if len(predobs.id) ==  0 :
+                    #     continue
+                    # print(predobs.id)
+
+                    # id , id_time= predobs.id.rsplit("_",1)
+                    # print("id :" , id)
+                    # if id_time in ['1s' , '2s' , '3s']:
+                    #     print(id , id_time)
+                if syncobs.sync_pred_obs_one != None: 
+                    syncobs.get_sync_timestamp()
+                    syncobs.get_sync_id()
+                    print('sync timestamp:' ,syncobs.sync_timestamp,syncobs.sync_id)
+                    print('syncobs finish',syncobs.sync_pred_obs_one)
+                         
+                        # print("labels :" , predobs.label)
+                        #  = queue.Queue()      
+                        # print(predobs.timestamp)
             # print(line)
-
-            
-
-#         data.append(list(line.strip().split(',')))
-
 
--- predictobstacle.py
+++ predictobstacle.py
@@ -31,9 +31,65 @@ class PredictObstacle(object):
         """
         logging.info(obstacle_info)
 
-        self.id: int = obstacle_info['id']
+        self.id_info: int = obstacle_info['id']
+        self.id: int = None
+        self.id_time = None 
         self.timestamp: int = int(float(obstacle_info["stamp"])*1000)
         self.label: float = obstacle_info["label"]
         self.position_x: float = obstacle_info["pose"]["position"]["x"]
         self.position_y: float = obstacle_info["pose"]["position"]["y"]
         self.obstacle_type: int = obstacle_info['type']
+    
+    def get_id_time(self):
+        if len(self.id_info) !=  0:
+            id , id_time = self.id_info.rsplit("_",1)[0],self.id_info.rsplit("_",1)[1]
+            if  id_time in ['1s' , '2s' , '3s']:
+                self.id , self.id_time = id , id_time
+                # return 
+
+
+class SyncPredictObstacle(object):     
+
+    def __init__(self, obs_number):
+        """Init
+
+        Args:
+            obstacle_info: obstacle basic info from raw data
+            mode: insert obstacle mode, eg: predicted, ground_truth
+        """ 
+        self.obs_number = obs_number
+        self.sync_pred_obs_one = None
+        self.sync_pred_obs_two = None
+        self.sync_pred_obs_three = None
+        self.sync_timestamp = None 
+        self.sync_id = None
+
+    def add_predict_obs(self,predictobs):
+        if predictobs.id_time =='1s':
+            self.sync_pred_obs_one = predictobs
+        if predictobs.id_time =='2s':
+            self.sync_pred_obs_two = predictobs
+        if predictobs.id_time =='3s':
+            self.sync_pred_obs_three = predictobs
+
+    def get_sync_timestamp(self):
+        if self.sync_pred_obs_one.timestamp == self.sync_pred_obs_two.timestamp \
+         == self.sync_pred_obs_three.timestamp:
+            self.sync_timestamp = self.sync_pred_obs_one.timestamp
+        else :
+            pass
+
+    def get_sync_id(self):
+        if self.sync_pred_obs_one.id == self.sync_pred_obs_two.id \
+         == self.sync_pred_obs_three.id:
+            self.sync_id = self.sync_pred_obs_one.id
+        else :
+            pass
+
+
+
+        
+        # self.sync_ptred_obs.append(predictobs)
+
+
+
